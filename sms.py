227c41d6c573aa017c125ca75e6336b20f6fb54b
Peter Helenefors <peter@hertigcarl.se>
2014-12-21 16:08:57 +0100

    cleaned up and corrected comments

--- sms.py
+++ sms.py
@@ -1,18 +1,21 @@
 # This is pyserial which is needed to communicate with the dongle
-# coding=utf-8
 import serial
 import io
 import binascii
 import sys
-# Set up the connection to the dongle
 
 number = sys.argv[1]
 message = sys.argv[2]
+
+# Make sure the message is ASCII friendly. Throws error if not.
 message.decode('ascii')
 
+# Set up the connection to the dongle
 dongle = serial.Serial(port="/dev/tty.HUAWEIMobile-Modem",baudrate=115200,timeout=2,rtscts=0,xonxoff=0)
-# sio = io.TextIOWrapper(io.BufferedRWPair(dongle, dongle))
+
 print "Connected!"
+
+# Wait for response. Must be called after each execution that returns an answer.
 def getResponse():
     sb = []
     sb.append(dongle.read())
@@ -20,39 +23,17 @@ def getResponse():
     return ''.join(sb)
 
 
-# This sends the command to the dongle
-# put the dongle into text mode
+# Tell dongle not to echo back commands.
 dongle.write('ATE0\r')
 print getResponse()
-# dongle.write('AT+CMGF?\r')
-# print getResponse()
-# read()
-# time.sleep(0.5)
-# dongle.flush()
-# sio.write(unicode("AT\n"))
-# sio.flush()
-# print sio.readline()
 
-#dongle.write('\r\r\rAT Z\r')
-#print getResponse()
-
+# Put dongle in text mode
 dongle.write('AT+CMGF=1\r')
 print getResponse()
 
-# dongle.write('AT+CSCS=?\r')
-# print getResponse()
-
-# dongle.write('AT+CSCS="USC2"')
-# print getResponse()
-
-# print 'Number = ' + binascii.hexlify('0739332499'.encode('utf16'))
-
 # Set the telephone number we want to send to
-dongle.write('AT+CMGS="' + '0739332499' + '"\r')
+dongle.write('AT+CMGS="' + number + '"\r')
 print getResponse()
-
-# print 
-
 
 #### Set the message we want to send
 try:
@@ -63,5 +44,6 @@ except:
 # Pass the CTRL+Z character to let the dongle know we're done
 dongle.write(chr(26))
 print getResponse()
+
 # Close the connection
 dongle.close()
